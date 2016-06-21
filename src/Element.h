461b9d39accdb4a3df2d6d41e9d0a007e94e63f4
yassermatos <yasselmatos01@gmail.com>
2016-06-20 23:35:11 -0400

    Update Element.h

--- src/Element.h
+++ src/Element.h
@@ -11,11 +11,20 @@
 class Element
 {
 	private:
-		std::string _name;
+		std::string _nombre;
+		std::string _autor;
+		std::string _editorial;
+		std::string _pDate;
+		std::string _categoria;
 		Element* _next;
 
 	public:
-		Element(std::string name);
+		Element(std::string nombre, std::string autor, std::string editorial, std::string pDate, std::string categoria);
+		std::string getTitle(){return _nombre;}
+		std::string getAuthor(){return _autor;}
+		std::string getPublisher(){return _editorial;}
+		std::string getDate(){return _pDate;}
+		std::string getCategory(){return _categoria;}
 	//This means that Stack can access directly to Element's private members
 	friend class Stack;
 };
